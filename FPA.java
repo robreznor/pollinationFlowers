4f125eec8af2228944eaeccf6f0fb60f1aa1fd96
Robreznor <r.macleanm@alumnos.uv.cl>
2016-11-26 21:05:37 -0400

    update1.0

--- FPA.java
+++ FPA.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package flowerspolination;
+package pollinationFlowers;
 
 import java.text.DecimalFormat;
 import java.util.Random;
@@ -15,17 +15,22 @@ import java.util.Random;
 public class FPA {
     int t, maxGeneration, n;
     int bestx1Pos, bestx2Pos;
+    int[] sol;
+    int[][] restricciones;;
     float p, gBest[],gBBest[],BBest,s, next_x1[],next_x2[],x1[],x2[];
     GenerateFlowers flowers;
     Random rand;
     String path;
     
-    public FPA(int maxGeneration, int n){   //asignación de variables
+    public FPA(int maxGeneration, int n){   
+//asignación de variables
         VarCons varcons= new VarCons();
-        String path="F:\\Universidad\\segundo semestre 2016\\fia\\presentación 2\\input\\scp41.txt";
+        String path="E:\\uvalpo\\2do Semestre 2016\\fia\\tarea 2\\input\\scp41.txt";
         int[][] var=varcons.generate(path);
         this.maxGeneration=maxGeneration;
         this.n = n;
+        this.restricciones = new int[][]{{1,1,0,0,0,0,0}, {1,1,1,1,0,0,1}, {0,1,1,1,0,0,0}, {0,1,1,1,1,0,1}, {0,0,0,1,1,1,1}, {0,0,0,0,1,1,0}, {0,1,0,1,1,0,1}};
+        sol=new int[7];
         //flowers=new GenerateFlowers();      //se genera la población
         //x1=flowers.Generate(n,8);
         //x2=flowers.Generate(n,(float)5.33);
@@ -33,38 +38,44 @@ public class FPA {
         rand = new Random();
         s=100;
         gBBest= new float[2];
-        next_x1=new float[x1.length];
-        next_x2=new float[x2.length];               
+        //next_x1=new float[x1.length];
+        //next_x2=new float[x2.length];               
     }
     /**
      * este metodo hace la pega
      */
     public void Algorithm(){
         
-        gBest= GBest();                     //Mejor solución inicial
+  //      gBest= GBest();                     //Mejor solución inicial
+        this.min(restricciones);
+        for(int i=0;i<restricciones.length;i++){
+            System.out.println("cumple: "+this.Cover(restricciones[i],restricciones.length));
+            
+        }
+        
         while(t < maxGeneration){           //Iteraciones que se ejecuta el algoritmo
             for(int i=0;i<n;i++){
                 
                 if(rand.nextFloat()<p){ 
-                    next_x1[i]=x1[i]+ Levy()*(gBest[0]-x1[i]);      //La planta es del tipo biotico por lo tanto el transporte se realiza mediante una dist. de Levy                
+ //                   next_x1[i]=x1[i]+ Levy()*(gBest[0]-x1[i]);      //La planta es del tipo biotico por lo tanto el transporte se realiza mediante una dist. de Levy                
                 }
                 else{
-                   next_x2[i]=x2[i]+ rand.nextFloat()*(Math.abs(x2[(int)(rand.nextDouble()*n-1)]-x2[(int)(rand.nextDouble()*n-1)])); //Transporte mediante una dist. Uniforme 
+//                   next_x2[i]=x2[i]+ rand.nextFloat()*(Math.abs(x2[(int)(rand.nextDouble()*n-1)]-x2[(int)(rand.nextDouble()*n-1)])); //Transporte mediante una dist. Uniforme 
 
                 }
             }
-            gBest= GBest();
+ //           gBest= GBest();
      
-            if(GBestValue()>BBest){  //Si el nuevo valor tiene mejor fitness se selecciona como mejor solución 
+   /*         if(GBestValue()>BBest){  //Si el nuevo valor tiene mejor fitness se selecciona como mejor solución 
                 gBBest = gBest;
                 BBest=GBestValue();
             } 
-            
+     */       
             x1=next_x1;  
             x2=next_x2;
             t++;           
         }
-        sol();
+        Sol();
     }
     
     private float Levy(){ //Calcula la distrib. de Levy
@@ -72,7 +83,7 @@ public class FPA {
         return levy;
     }
 
-    public float[] GBest(){                     //Calcula la mejor solución de una iteración.
+  /*  public float[] GBest(){                     //Calcula la mejor solución de una iteración.
         float best=0,sol;
         float[] bests= new float[2];
         for(int i=0;i<x1.length;i++){
@@ -89,8 +100,40 @@ public class FPA {
     public float GBestValue(){                  //Evalua la función objetivo. 
         return gBest[0]*40000+gBest[1]*45000;
     }
+    */
     
-    public void showPop(){              
+    public void min(int[][] restricciones){
+        int min;
+        for(int i=0;i<restricciones.length;i++){
+            min=10000;
+            for(int j=0;j<restricciones[i].length;j++){
+                if(restricciones[i][j]>=1){
+                    if(restricciones[i][j]<min){
+                        min=restricciones[i][j];
+                        sol[j]=1;
+                    }
+                }     
+            }                
+        }
+        
+        for(int i=0;i<sol.length;i++){
+            System.out.println(""+sol[i]);
+        }
+    }
+    
+    public int Cover(int[] restriccion,int length){
+        int acumulado=1;
+        if(length-1<1) return acumulado;
+        else{
+            acumulado=acumulado*restriccion[length-1];
+            length--;
+            return acumulado;
+        }
+            
+        
+    }
+    
+    public void ShowPop(){              
         for(int i=0;i<x1.length;i++){           //Muesta la población
             System.out.print("x1: "+x1[i]+" ");
         }
@@ -108,7 +151,7 @@ public class FPA {
    
     }
     
-    public void sol(){                          //Imprime la solución del problema
+    public void Sol(){                          //Imprime la solución del problema
         DecimalFormat decimales = new DecimalFormat("0.000");
         System.out.println("["+gBBest[0]+"]["+gBBest[1]+"] = "+ decimales.format(gBBest[0]*40000+45000*gBBest[1]));
         
